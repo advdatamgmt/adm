ee7f54679688279d20ec88592e60aed012f70523
Beau Benjamin Bruce <bbruce@gmail.com>
2015-10-21 22:02:44 -0400

    Added html function

---
+++ R/html.R
@@ -0,0 +1,23 @@
+#' Create HTML quickly
+#'
+#' @param value is what goes inside the tag
+#' @param tag is a valid HTML tag
+#' @param attr is a list of attributes
+html <- function(value, tag, attr = NULL) {
+  # paste the attributes/values together, attr needs to be a list of lists
+  # allows recycling of arguments
+  makeattrlist <- function(a) {
+    out <- " "
+    if (length(a) > 0) {
+      out <- paste(mapply(
+        function(...) paste(..., sep = ''), names(a), '="' ,a, '"'),
+        collapse = " ")
+      out <- paste(" ", out, sep = '') # add leading space
+    }
+    out
+  }
+  attrlist <- sapply(attr, makeattrlist)
+  attrlist <- sub('=""' , "", attrlist, fixed = TRUE)
+  paste("<", tag, attrlist, ">", value, "</", tag, ">\n" ,sep = ''
+        , collapse = '')
+}
